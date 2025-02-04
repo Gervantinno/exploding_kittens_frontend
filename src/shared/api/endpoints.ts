02d8d8822597397a1c461c8b664ab8ee38463fa8
Turcan Valerii <nadoborotsa@gmail.com>
2025-02-04 16:01:23 +0300

    kt2 updates

--- src/shared/api/endpoints.ts
+++ src/shared/api/endpoints.ts
@@ -1,3 +1,4 @@
+import { IPlayer } from '../utils/getGameState';
 import socket from './socket';
 
 //eslint-disable-next-line
@@ -5,12 +6,23 @@ export function subcribeGame(callback: (state: any) => void) {
   socket.on('update-game', callback);
 }
 
-export function unsubscribeGame() {
+export interface ILeaveGame {
+  gameId: string;
+  newPlayer: IPlayer;
+}
+
+export function unsubscribeGame(body: ILeaveGame) {
   socket.off('update-game');
+  socket.emit('leave-game', body);
+}
+
+export interface IConnectToGame {
+  gameId: string;
+  newPlayer: IPlayer;
 }
 
 //eslint-disable-next-line
-export function connectToGame(name: string, callback: (state: any) => void) {
-  socket.emit('join-game', name);
+export function connectToGame(body: IConnectToGame, callback: (state: any) => void) {
   subcribeGame(callback);
+  socket.emit('join-game', body);
 }
